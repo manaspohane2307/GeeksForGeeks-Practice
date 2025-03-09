45f3b3cf9418766ad1815a559e68e969eb08617e
Manas Pohane <manaspohane@gmail.com>
2025-03-09 12:51:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome SubStrings/palindrome-sub-strings.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String s = sc.next();
+
+            Solution obj = new Solution();
+
+            System.out.println(obj.countPS(s));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    public int countPS(String s) {
+        // code here
+        int n = s.length();
+        int count=0;
+        for(int i=0;i<n;i++){
+            int l=i;
+            int r=i;
+            while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
+                if(r-l+1 >= 2){
+                    count++;
+                }
+                l-=1;
+                r+=1;
+            }
+            l=i;
+            r=i+1;
+            while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
+                if(r-l+1 >= 2){
+                    count++;
+                }
+                l-=1;
+                r+=1;
+            }
+        }
+        return count;
+    }
+}
