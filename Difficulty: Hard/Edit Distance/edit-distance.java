9c9978e02ab8544ad791809916f41528310bdbc5
Manas Pohane <manaspohane@gmail.com>
2025-03-10 20:14:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s1 = br.readLine();
+            String s2 = br.readLine();
+
+            Solution ob = new Solution();
+            int ans = ob.editDistance(s1, s2);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int editDistance(String s1, String s2) {
+        // Code here
+        int n = s1.length();
+        int m = s2.length();
+
+        // Create a 2D array to store the minimum edit distances
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Initialize the first row and column with their respective indices
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = j;
+        }
+
+        // Fill the dp array using a bottom-up approach
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    // If the characters match, no edit is needed, so take the value from the diagonal.
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    // If the characters don't match, take the minimum of three possibilities:
+                    // 1. Replace the character in S1 with the character in S2 (diagonal).
+                    // 2. Delete the character in S1 (left).
+                    // 3. Insert the character from S2 into S1 (up).
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
