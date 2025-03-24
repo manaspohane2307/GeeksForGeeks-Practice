e1e565e8efc767bf56ff670fa08ee35b678a0401
Manas Pohane <manaspohane@gmail.com>
2025-03-24 12:07:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of 2 Sorted with Duplicates/union-of-2-sorted-with-duplicates.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            // First array input
+            String[] str1 = br.readLine().trim().split(
+                " "); // Read the first line and split by spaces
+            int n = str1.length;
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str1[i]); // Convert each element to an integer
+            }
+
+            // Second array input
+            String[] str2 = br.readLine().trim().split(
+                " "); // Read the second line and split by spaces
+            int m = str2.length;
+            int[] b = new int[m];
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(str2[i]); // Convert each element to an integer
+            }
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = obj.findUnion(a, b);
+            for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+// a,b : the arrays
+class Solution {
+    // Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        // add your code here
+        int n1 = a.length;
+        int n2 = b.length;
+        
+        int i=0,j=0; //i and j points to start index of both arrays
+        ArrayList<Integer> union = new ArrayList<>();
+
+        while(i<n1 && j<n2){
+            if(a[i] <= b[j]){//case1 and case2
+                if(union.size() == 0 || union.get(union.size()-1)!=a[i]){
+                    union.add(a[i]);
+                }
+                i++;
+            }
+            else{//case3
+                if(union.size() == 0 || union.get(union.size()-1)!=b[j]){
+                    union.add(b[j]);
+                }
+                j++;
+            }
+        }
+        //if any elements left in arr1
+        while(i<n1){
+            if(union.get(union.size()-1)!=a[i]){
+                union.add(a[i]);
+            }
+            i++;
+        }
+        //if any elements left in arr2
+        while(j<n2){
+            if(union.get(union.size()-1)!=b[j]){
+                union.add(b[j]);
+            }
+            j++;
+        }
+        return union;
+        
+    }
+}
